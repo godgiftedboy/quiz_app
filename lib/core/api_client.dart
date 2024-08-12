4f7a69fe98c22781595277788abd72fbefa42b62
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-08-12 07:59:23 +0545

    chore: Api client class added with exception handling

---
+++ lib/core/api_client.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:quiz_app/core/api_const.dart';
+import 'package:quiz_app/core/api_method_enum.dart';
+import 'package:quiz_app/core/exception_handle.dart';
+
+class ApiClient {
+  Future request({
+    required String path,
+    ApiMethod type = ApiMethod.get,
+    dynamic data = const {},
+  }) async {
+    final Dio dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConst.baseUrl,
+      ),
+    );
+
+    try {
+      final result = type == ApiMethod.get
+          ? await dio.get(path)
+          : type == ApiMethod.put
+              ? await dio.put(path, data: data)
+              : type == ApiMethod.delete
+                  ? await dio.delete(path)
+                  : type == ApiMethod.patch
+                      ? await dio.patch(path, data: data)
+                      : await dio.post(path, data: data);
+      return result.data;
+    } on DioException catch (e) {
+      throw DioExceptionHandle.fromDioError(e);
+    }
+  }
+}
