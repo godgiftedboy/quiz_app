ce9ed716fb921d3fcc88c262dc23ac4bd4a27f11
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-08-13 16:21:40 +0545

    refactor: Implemented AsyncNotifier inplace of StateNotifier

--- lib/features/quiz/presentation/logic/question_controller.dart
+++ lib/features/quiz/presentation/logic/question_controller.dart
@@ -1,19 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quiz_app/features/quiz/data/model/question_model.dart';
 import 'package:quiz_app/features/quiz/data/repository/question_repository.dart';
 
-class QuestionController
-    extends StateNotifier<AsyncValue<List<QuestionModel>>> {
-  final QuestionRepository _questionRepository;
-  QuestionController(this._questionRepository)
-      : super(const AsyncValue.loading()) {
-    getAllQuestions();
+// class QuestionController
+//     extends StateNotifier<AsyncValue<List<QuestionModel>>> {
+//   final QuestionRepository _questionRepository;
+//   QuestionController(this._questionRepository)
+//       : super(const AsyncValue.loading()) {
+//     getAllQuestions();
+//   }
+//   getAllQuestions() async {
+//     final result = await _questionRepository.getQuestions();
+//     return result.fold(
+//       (l) {
+//         state = AsyncValue.error(l.message, StackTrace.fromString(l.message));
+//       },
+//       (r) {
+//         state = AsyncValue.data(r);
+//       },
+//     );
+//   }
+
+//   resetState() {
+//     state = const AsyncValue.data([]);
+//   }
+// }
+
+// final questionControllerProvider =
+//     StateNotifierProvider<QuestionController, AsyncValue<List<QuestionModel>>>(
+//   (ref) => QuestionController(
+//     ref.read(questionRepositoryProvider),
+//   ),
+// );
+
+class QuestionController extends AsyncNotifier<List<QuestionModel>> {
+  @override
+  FutureOr<List<QuestionModel>> build() {
+    return getAllQuestions();
   }
+
   getAllQuestions() async {
-    final result = await _questionRepository.getQuestions();
+    final result = await ref.read(questionRepositoryProvider).getQuestions();
     return result.fold(
       (l) {
         state = AsyncValue.error(l.message, StackTrace.fromString(l.message));
+        print(l.message);
       },
       (r) {
         state = AsyncValue.data(r);
@@ -27,8 +60,5 @@ class QuestionController
 }
 
 final questionControllerProvider =
-    StateNotifierProvider<QuestionController, AsyncValue<List<QuestionModel>>>(
-  (ref) => QuestionController(
-    ref.read(questionRepositoryProvider),
-  ),
-);
+    AsyncNotifierProvider<QuestionController, List<QuestionModel>>(
+        QuestionController.new);
